d176368c4444597c6cd8c84f0212609af7efc0db
marianadpereira <mariana.pereira@HMCTS.NET>
2025-12-30 15:16:45 +0000

    adding eslint and pipeline config

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // Node backend files (CommonJS)
+  {
+    files: ["src/azure-sa/**/*.js"], // adjust if your backend folder is named differently
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script", // CommonJS
+      globals: {
+        ...globals.node,
+        __dirname: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+
+  // Frontend/browser files
+  {
+    files: ["src/azure-sa/public/**/*.js"], // all browser JS
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module", // ESM
+      globals: {
+        ...globals.browser, // document, window, alert, etc.
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+]);
