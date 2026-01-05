18687dc97ca68f2d9cd295e8ceca0a83851bf515
marianadpereira <mariana.pereira@HMCTS.NET>
2026-01-05 18:07:45 +0000

    changing deployment to aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,84 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled  = false
 }
 
+resource "azurerm_monitor_workspace" "amw" {
+  name                = var.monitor_workspace_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                              = var.grafana_dashboard_name
+  resource_group_name               = var.resource_group_name
+  location                          = var.location
+  grafana_major_version             = 11
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_monitor_workspace_integrations {
+    resource_id = azurerm_monitor_workspace.amw.id
+  }
+}
+
+resource "azurerm_role_assignment" "grafana_monitor_reader" {
+  scope                = azurerm_monitor_workspace.amw.id
+  role_definition_name = "Monitoring Data Reader"
+  principal_id         = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+}
+
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = var.log_analytics_workspace_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_diagnostic_setting" "webapp_diagnostics" {
+  name                       = "web-app-logs-to-law"
+  target_resource_id = "/subscriptions/680a7707-ec64-4b22-a46a-8747188752b6/resourceGroups/FileVault/providers/Microsoft.Web/sites/filevault-app"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+
+  enabled_metric {
+    category = "AllMetrics"
+  }
+}
+
+resource "azurerm_role_assignment" "grafana_log_reader" {
+  scope                = azurerm_log_analytics_workspace.logs.id
+  role_definition_name = "Log Analytics Reader"
+  principal_id         = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+}
+
+resource "azurerm_kubernetes_cluster" "filevault_aks" {
+  name                = var.filevault-aks-cluster_name
+  location            = "northeurope"
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "filevaultaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_to_acr" {
+  principal_id                     = azurerm_kubernetes_cluster.filevault_aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,23 @@ variable "kv_name" {
   description = "Key Vault name"
   default     = "filevault-kv-mariana"
 }
+
+variable "monitor_workspace_name" {
+  description = "Monitor Workspace name"
+  default     = "filevault-monitor-workspace"
+}
+
+variable "grafana_dashboard_name" {
+  description = "Grafana Dashboard name"
+  default     = "filevault-grafana"
+}
+
+variable "log_analytics_workspace_name" {
+  description = "Log Analytics Workspace name"
+  default     = "filevault-logs-workspace"
+}
+
+variable "filevault-aks-cluster_name" {
+  description = "AKS Cluster name"
+  default     = "filevault-aks-cluster"
+}
